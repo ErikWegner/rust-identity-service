c477694c895378fac8983c190a5f6d2b64e1473a
Erik Wegner <E_Wegner@web.de>
2021-12-12 22:52:32 +0000

    HealthMap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,59 @@
-use rocket::{Build, Rocket};
+use std::{collections::HashMap, sync::Arc};
+
+use dashmap::DashMap;
+use rocket::serde::{Deserialize, Serialize};
+use rocket::{
+    http::Status,
+    response::{content, status},
+    Build, Rocket, State,
+};
 
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+type HealthMap = DashMap<String, String>;
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct HealthResponse {
+    faults: HashMap<String, String>,
+}
+
+#[get("/up")]
+fn up() -> &'static str {
+    "OK"
+}
+
+#[get("/health")]
+fn health(
+    healthmap: &State<Arc<HealthMap>>,
+) -> Result<&'static str, status::Custom<content::Json<String>>> {
+    let healthmap_iter = healthmap.iter();
+    let faults = healthmap_iter
+        .filter(|e| e.value() != "OK")
+        .map(|e| (e.key().clone(), e.value().clone()))
+        .collect::<HashMap<_, _>>();
+
+    if faults.is_empty() {
+        return Ok("OK");
+    }
+    let j = HealthResponse { faults };
+    Err(status::Custom(
+        Status::BadGateway,
+        content::Json(serde_json::to_string(&j).unwrap()),
+    ))
+}
+
+fn build_rocket_instance(healthmap: Arc<HealthMap>) -> Rocket<Build> {
+    rocket::build()
+        .manage(healthmap)
+        .mount("/", routes![up, health])
 }
 
 #[launch]
-fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![index])
+fn rocket() -> _ {
+    let healthmap = Arc::new(HealthMap::new());
+    build_rocket_instance(healthmap)
 }
 
 #[cfg(test)]
--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,56 @@
+use std::sync::Arc;
+
+use crate::{build_rocket_instance, HealthMap};
+
 use super::rocket;
 use rocket::http::Status;
 use rocket::local::blocking::Client;
+use rocket::{Build, Rocket};
+
+struct TestEnv {
+    rocket: Rocket<Build>,
+    health_map: Arc<HealthMap>,
+}
+
+fn build_rocket_test_instance() -> TestEnv {
+    let health_map = Arc::new(HealthMap::new());
+    TestEnv {
+        rocket: build_rocket_instance(health_map.clone()),
+        health_map,
+    }
+}
 
 #[test]
-fn hello_world() {
-    let client = Client::tracked(rocket()).expect("valid rocket instance");
-    let response = client.get("/").dispatch();
+fn up() {
+    let t = build_rocket_test_instance();
+    let client = Client::tracked(t.rocket).expect("valid rocket instance");
+    let response = client.get("/up").dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.into_string(), Some("Hello, world!".into()));
+    assert_eq!(response.into_string(), Some("OK".into()));
+}
+
+#[test]
+fn health_simple_ok() {
+    let t = build_rocket_test_instance();
+    t.health_map.clear();
+    t.health_map.insert("con".to_string(), "OK".to_string());
+    let client = Client::tracked(t.rocket).expect("valid rocket instance");
+    let response = client.get("/health").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.into_string(), Some("OK".into()));
+}
+
+#[test]
+fn health_simple_fail() {
+    let t = build_rocket_test_instance();
+    t.health_map.clear();
+    t.health_map
+        .insert("con".to_string(), "failed to connect".to_string());
+    let client = Client::tracked(t.rocket).expect("valid rocket instance");
+    let response = client.get("/health").dispatch();
+    assert_eq!(response.status(), Status::BadGateway);
+    assert_eq!(
+        response.into_string(),
+        Some("{\"faults\":{\"con\":\"failed to connect\"}}".into())
+    );
 }
